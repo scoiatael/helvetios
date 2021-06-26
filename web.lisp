9d19418643bfc77377eb7db834989f41e554725b
Lukasz Czaplinski <git@scoiatael.dev>
2021-06-26 20:59:00 +0200

    webEngineViewLoad works
    There's something odd about its argument - when passing LISP variable it
    won't work. But works fine with object fields - because of null padding?

--- web.lisp
+++ web.lisp
@@ -8,16 +8,17 @@
 (defun queue-in-main-thread (f)
   (trivial-main-thread:call-in-main-thread f))
 
-(defclass webview-window ()
-    ((qt-widget :initform (cl-webengine:new-q-widget) :reader qt-widget)
-     (qt-layout :initform (cl-webengine:new-qv-box-layout) :reader qt-layout)
-     (qt-webview :initform (cl-webengine:new-q-web-engine-view) :reader qt-webview)))
+(defclass qt-browser ()
+  ((qt-widget :initform (cl-webengine:new-q-widget) :reader qt-widget)
+   (qt-layout :initform (cl-webengine:new-qv-box-layout) :reader qt-layout)
+   (qt-webview :initform (cl-webengine:new-q-web-engine-view) :reader qt-webview)
+   (url :initarg :url :reader current-url)))
 
 (defun web (window)
   (cl-webengine:layout-add-widget (qt-layout window) (qt-webview window))
   (cl-webengine:widget-set-layout (qt-widget window) (qt-layout window))
   (log:info "Layout created")
-  (cl-webengine:web-engine-view-load (qt-webview window) "https://www.duckduckgo.com")
+  (cl-webengine:web-engine-view-load (qt-webview window) (current-url window))
   (log:info "Webview loaded")
   (cl-webengine:widget-show (qt-widget window)))
 
@@ -31,7 +32,7 @@
 (defun run ()
   (let ((application
           (apply #'cl-webengine:new-q-application (alloc-argv :string "helvetios")))
-        (window (make-instance 'webview-window)))
+        (window (make-instance 'qt-browser :url "https://joemonster.org")))
     (log:info "Application created")
     (setf *application* application)
     (web window)
